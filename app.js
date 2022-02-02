075d4d5a26f31d25b25ef34bb188832e87a9daa3
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-02 16:37:20 +0100

    ceated add and get todos

--- app.js
+++ app.js
@@ -13,6 +13,32 @@ app.get("/", (req, res) => {
   res.status(200).json("Everithing is working ok");
 });
 
+//create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { desc } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [desc]
+    );
+    res.json(newTodo.rows);
+  } catch (err) {
+    console.log(`Ups there was an error: ${err}`);
+    res.json(err.message);
+  }
+});
+
+// GET ALL TODOS
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.log(`Ups there was an error: ${err}`);
+    res.json(err.message);
+  }
+});
+
 //start up
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
